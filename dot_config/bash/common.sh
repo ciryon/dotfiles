f5503d2bad9f17563775e22b4585eb47501c6ffb
Christian Hedin <christian.hedin@puls-solutions.se>
2025-09-29 21:10:29 +0200

    bash: update paths for mac

--- dot_config/bash/common.sh
+++ dot_config/bash/common.sh
@@ -2,8 +2,18 @@ if [[ "$(uname -s)" == "Linux" ]] && [[ -r ~/.local/share/omarchy/default/bash/r
   source ~/.local/share/omarchy/default/bash/rc
 fi
 
+
+
 set -h
 
 # Exports
 export DEVOPS_EVENTS_DIR="/home/ciryon/Coding/PulsSolutions/services/devops-events-service" # should move elsewhere
 export PATH="$HOME/Coding/PulsSolutions/scripts/bin:$PATH"
+
+
+case "$(uname -s)" in
+  Darwin)
+    HOMEBREW_PREFIX="/opt/homebrew"
+    export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
+    ;;
+esac
