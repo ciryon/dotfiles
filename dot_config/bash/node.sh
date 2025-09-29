08eb1f17a12e3283e14e9d2563aac3b43994e32a
Christian Hedin <christian.hedin@puls-solutions.se>
2025-09-29 21:44:20 +0200

    alacritty and node fixes

--- dot_config/bash/node.sh
+++ dot_config/bash/node.sh
@@ -23,10 +23,40 @@ awsnpm() {
   npm config set registry "https://registry.npmjs.org/"
 }
 
-# NVM (Arch vs Homebrew)
+# NVM (Arch and Homebrew)
+
+# Linux (Arch)
 if [[ -r /usr/share/nvm/init-nvm.sh ]]; then
   source /usr/share/nvm/init-nvm.sh
+
+# macOS (Homebrew)
 elif [[ -r /opt/homebrew/opt/nvm/nvm.sh ]]; then
   export NVM_DIR="$HOME/.nvm"
   source /opt/homebrew/opt/nvm/nvm.sh
+  [[ -s /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm ]] &&
+    source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm
 fi
+
+# Bash completion if present
+[[ -s /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm ]] && source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm
+[[ -s /usr/local/opt/nvm/etc/bash_completion.d/nvm ]] && source /usr/local/opt/nvm/etc/bash_completion.d/nvm
+
+# Auto-use Node version from nearest .nvmrc (on cd and on new shells)
+nvm_auto_use() {
+  # nvm provides nvm_find_nvmrc; fall back to local .nvmrc
+  local nvmrc
+  nvmrc="$(command -v nvm_find_nvmrc >/dev/null 2>&1 && nvm_find_nvmrc || true)"
+  [[ -z "$nvmrc" && -f .nvmrc ]] && nvmrc="$PWD/.nvmrc"
+  [[ -z "$nvmrc" ]] && return 0
+
+  local required current
+  required="$(nvm version "$(cat "$nvmrc")")"
+  current="$(nvm version)"
+  [[ "$required" = "N/A" ]] && {
+    nvm install >/dev/null
+    return 0
+  }
+  [[ "$required" != "$current" ]] && nvm use >/dev/null
+}
+# run on each prompt (covers new shells + after cd)
+PROMPT_COMMAND="nvm_auto_use${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
