eb2a7a5a4e4e3244f05a3be00cf36e2852d008d1
Christian Hedin <christian.hedin@puls-solutions.se>
2025-09-30 22:13:07 +0200

    some aliases

--- dot_config/bash/10-aliases.sh
+++ dot_config/bash/10-aliases.sh
@@ -5,4 +5,6 @@ alias vim=nvim
 alias pino-pretty='pino-pretty -i hostname,pid -S -t "SYS:yyyy-mm-dd HH:MM:ss"' # hide hostname,pid + single line + timestamp
 alias lg=lazygit
 alias logs=puls_aws_logs
+alias js='cd ~/Coding/JavaScript'
+alias js='cd ~/Coding/Istari'
 alias hurrah="echo \"Hurrah!\""
