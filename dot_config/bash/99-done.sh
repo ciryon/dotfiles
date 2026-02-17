242a79f14aa072bc55d67ad551fe18047d1b6999
Christian Hedin <christian.hedin@puls-solutions.se>
2026-02-17 16:13:54 +0100

    bash ordering and pretty splash

--- dot_config/bash/99-done.sh
+++ dot_config/bash/99-done.sh
@@ -6,6 +6,19 @@ source <(carapace _carapace)
 [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
 eval "$(atuin init bash --disable-up-arrow)"
 
-{{ if (contains "omarchy" .chezmoi.hostname) }}
+
 echo "Remember: 'zi' for  cd, 'zellij' for terminal multiplexer - and yazi for file manager!"
-{{ end }}
+
+# A fun pic
+if command -v chafa >/dev/null 2>&1 && [[ -n "$DROPBOX_PERSONAL" ]]; then
+
+  if [[ -z "$PS1_SHOWN" ]]; then
+    export PS1_SHOWN=1
+    chafa --format=kitty --scale=max --align=center \
+      "$DROPBOX_PERSONAL/Images/telchar.png"
+  fi
+
+
+fi
+
+
